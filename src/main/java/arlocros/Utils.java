0e5253ee64f220333daa33b32f47c3cba78dd160
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-12-28 11:26:50 +0100

    Replace threshold filter by adaptive filter

--- src/main/java/arlocros/Utils.java
+++ src/main/java/arlocros/Utils.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2016 Marvin Ferber.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -55,7 +55,7 @@ public class Utils {
     final int col = 0;
     transformMat.put(row, col, 0.33, 0.33, 0.34);
     Core.transform(image2, image2, transformMat);
-    Imgproc.threshold(image2, image2, d / 3, 255, Imgproc.THRESH_BINARY);
+    Imgproc.adaptiveThreshold(image2, image2, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 161, 30);
     if (invertBlackWhiteColor) {
       Core.bitwise_not(image2, image2);
     }
